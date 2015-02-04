c571d6715276ba06b92cd19d65ec8b6b48f26d9c
XLEvolutionist <sbyfield@ucdavis.edu>
2015-02-03 17:48:23 -0800

    modified the slurm submission script

--- scripts/submitFREEC.sh
+++ scripts/submitFREEC.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-#OUTDIR=/group/jrigrp4/freec/output
+#OUTDIR=/group/jrigrp4/freec/config
 #SBATCH -D /group/jrigrp4/freec/output
 #SBATCH -o /group/jrigrp4/freec/logs/freec_out_log-%j.txt
 #SBATCH -e /group/jrigrp4/freec/logs/freec_err_log-%j.txt
 #SBATCH -J freec
-#SBATCH --mem=10000
+#SBATCH --mem=12000
 #SBATCH --cpus-per-task 10
-##SBATCH --array=0
+#SBATCH --array=0-20
+
+files=(*.conf)
+
+#cmd="samtools index ${files[$SLURM_ARRAY_TASK_ID]}"
 
 echo "Starting freec job: "
 date
 
-cmd="freec -conf ../config.txt"
+cmd="freec -conf ${files[$SLURM_ARRAY_TASK_ID]}"
 echo $cmd
 eval $cmd
 
