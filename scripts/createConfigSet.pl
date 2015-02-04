34aab386cbf99b49410c0cb78c145e9311542635
Simon Renny-Byfield <sbyfield@ucdavis.edu>
2015-02-03 17:28:54 -0800

    modified to make the output dir

--- scripts/createConfigSet.pl
+++ scripts/createConfigSet.pl
@@ -16,17 +16,17 @@ closedir $dir;
 # now create a freec config script for each bam file.
 ###
 foreach ( @files ) {
-
+	mkdir /group/jrigrp4/freec/output/$_;
 	open(OUT , ">config_$_.txt" ) || die;
 	print OUT "[general]
-
+	
 chrLenFile = /group/jrigrp4/freec/chrLenFile.txt
 coefficientOfVariation = 0.062
 ploidy = 2
 chrFiles=/group/jrigrp4/freec/maize3/
 maxThreads=10
 sex=XX
-
+outputDir=/group/jrigrp4/freec/$_
 #breakPointThreshold = -.002;
 window = 1000
 #degree=3
